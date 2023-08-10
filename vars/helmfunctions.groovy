d7f5fe02c88bb18bd5ccd8d16a3446e9c5287483
S0ulTak3r <DanielBusinessBoguslavsky@gmail.com>
2023-08-10 04:02:33 +0300

    l

--- vars/helmfunctions.groovy
+++ vars/helmfunctions.groovy
@@ -2,7 +2,6 @@ def packageHelmChart(String folder, String bucket, String bucketFolder) {
     // Check for changes in the mynewchart directory
     def chartChanges = sh(script: "git diff --name-only HEAD~1 HEAD | grep mynewchart || true", returnStdout: true).trim()
 
-    
     if (chartChanges) {
         // Determine the type of change
         def changeType = 'patch'  // default to patch
@@ -12,21 +11,18 @@ def packageHelmChart(String folder, String bucket, String bucketFolder) {
         } else if (chartChanges.contains("templates/")) {
             changeType = 'minor'
         } 
-        
+
         // Bump version
         sh "bash ${folder}/scripts/versionBump.sh ${changeType}"
 
-        // Extract the new version from Chart.yaml
-        def newVersion = sh(script: "grep '^version:' ${folder}/mynewchart/Chart.yaml | awk '{print \$2}'", returnStdout: true).trim()
+        // Extract the new version from Chart.yaml using awk
+        def newVersion = sh(script: "awk '/name: myproject/{getline; print \$2}' ${folder}/mynewchart/Chart.yaml", returnStdout: true).trim()
 
         // Predict the packaged chart name
         def packagedChartName = "myproject-${newVersion}.tgz"
 
         // Package helm chart using the mynewchart directory
         sh "helm package ${folder}/mynewchart -d ${folder}"
-
-        // Delete any previous charts from GCS bucket (to ensure only the latest is stored)
-        sh "gsutil rm gs://${bucket}/${bucketFolder}/myproject*.tgz"
 
         // Push the latest chart to GCS bucket, inside the specified folder
         sh "gsutil cp ${folder}/${packagedChartName} gs://${bucket}/${bucketFolder}/"
