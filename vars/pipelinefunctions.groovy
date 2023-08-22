c84bb6167393035248fb1aa2d67385aaa4f68f97
S0ulTak3r <DanielBusinessBoguslavsky@gmail.com>
2023-08-22 15:11:21 +0300

    l

--- vars/pipelinefunctions.groovy
+++ vars/pipelinefunctions.groovy
@@ -41,6 +41,49 @@ def checkChanges(String jenkinsfile)
     }
 }
 
+def checkChanges()
+{
+    try
+    {
+        echo "Checking for changes..."
+        def changeSets = currentBuild.changeSets
+        if (changeSets.size() == 0)
+        {
+            println('No commits have been made. Proceeding with pipeline execution...')
+            env.NO_CHANGES = "false"
+        } 
+        else 
+        {
+            def modifiedFiles = []
+            for(changeSet in changeSets) 
+            {
+                for(item in changeSet) 
+                {
+                    modifiedFiles += item.getAffectedPaths()
+                }
+            }
+            modifiedFiles = modifiedFiles.minus("jenkinsfile")
+            
+            if (modifiedFiles.isEmpty()) 
+            {
+                println('Skipping pipeline execution as the only change is to the Jenkinsfile.')
+                env.NO_CHANGES = "true"
+            }
+            else
+            {
+                env.NO_CHANGES = "false"
+            }
+        }
+    }
+    catch (Exception e) 
+    {
+        echo "[ERROR]: ${e.getMessage()}"
+        currentBuild.result = 'FAILURE'
+        error "Failed To Check Changes"
+    }
+}
+
+
 def cleanupWorkspace() {
     try {
         cleanWs()
